a9f42a3b4937b03570e9b651f1201a71aeaf6b16
dariapolushkina <mioyasumi@gmail.com>
2025-02-16 19:43:34 +0300

    ok

---
+++ 2/java-script.js
@@ -0,0 +1,68 @@
+// // Задача 1
+// const age = prompt("Введите ваш возраст:");
+// if (age < 65) {
+//     console.log("Вам ещё рано на пенсию")
+// } else {
+//     console.log("Поздравляем с пенсионным возрастом")
+// }
+
+// // Задача 2
+// const num1 = prompt("Введите первое число:");
+// const num2 = prompt("Введите второе число:");
+
+// if (num1 > num2) {
+//     console.log("Первое число больше")
+// } else if (num1 < num2) {
+//     console.log("Второе число больше")
+// } else {
+//     console.log("Числа равны")
+// }
+
+// // Задача 3
+// const number = prompt("Введите число:");
+// if (number % 2 === 0) {
+//     console.log("Четное число")
+// } else {
+//     console.log("Нечетное число")
+// }
+
+// // Задача 4
+// const secret = 7;
+// const ugadai = prompt("Угадайте число от 1 до 10:");
+
+// if (ugadai == secret) {
+//     console.log("Вы угадали!");
+// } else {
+//     console.log("Попробуйте ещё раз!");
+// }
+
+
+// // Задача 5
+// const login = prompt("Логин:");
+// const password = prompt("Пароль:");
+// if (login === "admin" && password === "12345") {
+//     console.log("Добро пожаловать!")
+// } else {
+//     console.log("Неверный логин или пароль")
+// }
+
+
+// // Задача 6
+// const year = prompt("Введите год:");
+
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log("Это високосный год")
+// } else {
+//     console.log("Это не високосный год")
+// }
+
+
+// // Задача 7
+// const num = prompt("Введите число:");
+// if (num > 100) {
+//     console.log("Большое число")
+// } else if (num < 100) {
+//     console.log("Маленькое число")
+// } else {
+//     console.log("Точно 100!")
+// }
---
+++ 4/java-script.js
@@ -0,0 +1,11 @@
+
+// document.addEventListener("mousemove", function(event) {
+//     const coord = document.getElementById("coordinati");
+//     coord.textContent = `X: ${event.clientX}, Y: ${event.clientY}`;
+
+//     if (event.clientX > 300) {
+//         coord.classList.add("cvet")
+//     } else {
+//         coord.classList.remove("cvet")
+//     }
+// });
